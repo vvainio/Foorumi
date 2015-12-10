293f68a370ede4ca9a7f92811b6c0248a13b7bdc
Ville Vainio <vivainio@cs.helsinki.fi>
2015-12-10 21:22:22 +0200

    Week 7 / Exercise 53

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,14 @@
 FoorumApp.controller('ShowMessageController', function ($scope, $routeParams, Api) {
-  // Toteuta kontrolleri tähän
+  Api.getMessage($routeParams.id).success(function (message) {
+    $scope.message = message;
+  });
+
+  $scope.addReply = function () {
+    Api.addReply({
+      content: $scope.newReply.content,
+    }, $routeParams.id).success(function (reply) {
+      $scope.newReply.content = "";
+      $scope.message.Replies.push(reply);
+    });
+  };
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -2,4 +2,15 @@ FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $loc
   Api.getTopic($routeParams.id).success(function (topic) {
     $scope.topic = topic;
   });
+
+  $scope.addMessage = function () {
+    Api.addMessage({
+      title: $scope.newMessage.title,
+      content: $scope.newMessage.content
+    }, $routeParams.id).success(function (message) {
+      $scope.newMessage.title = "";
+      $scope.newMessage.content = "";
+      $scope.topic.Messages.push(message);
+    });
+  };
 });
