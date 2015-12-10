dae69983ff059443f18cb8b179e2dbe51e8767d2
Ville Vainio <vivainio@cs.helsinki.fi>
2015-12-10 20:32:26 +0200

    Week 7 / Exercise 51

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,3 +1,5 @@
 FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $location, Api) {
-  // Toteuta kontrolleri tähän
+  Api.getTopic($routeParams.id).success(function (topic) {
+    $scope.topic = topic;
+  });
 });
--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,14 @@
 FoorumApp.controller('TopicsListController', function ($scope, $location, Api) {
-  // Toteuta kontrolleri tähän
+  Api.getTopics().success(function (topics) {
+    $scope.topics = topics;
+  });
+
+  $scope.addTopic = function () {
+    Api.addTopic({
+      name: $scope.newTopic.name,
+      description: $scope.newTopic.description
+    }).success(function (topic) {
+      $location.path('/topics/' + topic.id);
+    });
+  };
 });
